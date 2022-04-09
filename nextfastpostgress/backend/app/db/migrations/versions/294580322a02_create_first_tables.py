53b5323d22f0da65c9bc70cba956dbc4e9d25ddd
makoxty <m.fukujuu@gmail.com>
2022-04-10 01:06:59 +0900

    first commit

---
+++ nextfastpostgress/backend/app/db/migrations/versions/294580322a02_create_first_tables.py
@@ -0,0 +1,26 @@
+"""create_first_tables
+
+Revision ID: 294580322a02
+Revises: 
+Create Date: 2022-04-09 16:02:20.769826
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+# revision identifiers, used by Alembic
+revision = '294580322a02'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
